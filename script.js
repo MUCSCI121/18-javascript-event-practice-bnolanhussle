51f59a791bcb1a3197df1a29b4b9db1e9acba8a4
bnolanhussle <155196261+bnolanhussle@users.noreply.github.com>
2024-03-25 21:55:50 +0000

    Mar 25, 2024, 9:55 PM

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
+
+
 function increaseValue(event) {
   let inputElement = document.getElementById("numValue");
   console.log("Original: ", inputElement.value);
   let newValue = inputElement.value + 1;
-  
+
   console.log("New Value: ", newValue);
 }
 
