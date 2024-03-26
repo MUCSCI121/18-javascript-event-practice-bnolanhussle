d44aa7dea3b4f107e5d0ccd6569044fe7dd266c5
bnolanhussle <155196261+bnolanhussle@users.noreply.github.com>
2024-03-26 22:09:10 +0000

    Mar 26, 2024, 10:09 PM

--- script.js
+++ script.js
@@ -1,53 +1,88 @@
-let inputValue = 0;
-let userEvents = [];
+// This will hold the value of the input field
+let currentValue = 0;
+// All log entries will be added to this array
+let logs = [];
 
-function increaseValue(event) {
-  let inputElement = document.getElementById("numValue");
-  let oldValue = inputValue;
-  inputValue = inputValue + 1;
-  inputElement.value = inputValue;
-  userEvents.push(
-    "User requested increase from " + oldValue + " to " + inputValue
-  );
+/** 
+  Updates the value displayed in the <input> element
+*/
+function updateDisplayedValue() {
+  const inputElement = document.getElementById("valueDisplay");
+  inputElement.value = currentValue;
 }
 
-function decreaseValue(event) {
-  let inputElement = document.getElementById("numValue");
-  let oldValue = inputValue;
-  if (inputValue > 0) {
-    inputValue = inputValue - 1;
-    inputElement.value = inputValue;
+/** 
+  Callback for when the user manually enters a value in the <input> element.  The event parameter
+  is passed in from the oninput event and allows us to access the value that was entered into the 
+  input element.
+*/
+function userInputValue(event) {
+  // Grab the new value and convert it from a string to an integer
+  const newValue = parseInt(event.target.value);
+  if (newValue >= 0) {
+    currentValue = newValue;
+    logs.push("User entered number, requested value " + newValue + ", set to " + currentValue);
+  } else {
+    currentValue = 0;
+    updateDisplayedValue();
+    logs.push("User entered invalid value " + event.target.value);
   }
-  userEvents.push(
-    "User requested decrease from " + oldValue + " to " + inputValue
-  );
 }
 
-function updateValue(event) {
-  let inputElement = document.getElementById("numValue");
-  let newValue = inputElement.value;
-  inputValue = parseInt(newValue);
-  if (inputValue < 0) {
-    inputValue = 0;
+/** 
+  Callback for when the user clicks the decrease button.  The value in the input element 
+  will be decreased by one as long as it doesn't go below zero.  
+*/
+function decreaseValue() {
+  let originalValue = currentValue;
+  if (currentValue > 0) {
+    currentValue--;
+    updateDisplayedValue();
+    logs.push("User requested decrease from " + originalValue + " to " + currentValue);
+  } else {
+    logs.push("User requested invalid decrease from " + originalValue + " to " + (originalValue - 1));
   }
-  inputElement.value = inputValue;
-  userEvents.push("User typed new value " + event.target.value);
 }
 
-function resetValue(event) {
-  inputValue = 0;
-  let inputElement = document.getElementById("numValue");
-  inputElement.value = inputValue;
-  userEvents.push("User requested a reset");
+/** 
+  Callback for when the user clicks the increase button.  Increases the value in the input element
+  by one
+*/
+function increaseValue() {
+  let originalValue = currentValue;
+  currentValue++;
+  updateDisplayedValue();
+
+  logs.push("User requested increase from " + originalValue + " to " + currentValue);
 }
 
-function displayLogs(event) {
-  let index = 0;
-  let displayText = "";
-  while (index < userEvents.length) {
-    displayText += userEvents[index] + "<br>";
-    index = index + 1;
-  }
-  let divLogs = document.getElementById("logs");
-  divLogs.innerHTML = displayText;
+/** 
+  Resets the value in the input element to zero and hides the log section
+*/
+function resetValue() {
+  logs.push("User requested reset at " + currentValue);
+  const divContainerElement = document.getElementById("logs");
+  divContainerElement.style.visibility = "hidden";
+
+  currentValue = 0;
+  updateDisplayedValue();
+}
+
+/** 
+  Updates the log section so that it is visible on the page
+*/
+function showLogs() {
+  logs.push("Show logs requested");
+  const divContainerElement = document.getElementById("logs");
+  divContainerElement.style.visibility = "visible";
+
+  // The array type has a function called join() that will combine all of the item values
+  // into one long string.  The value passed into the function is what will be used as the 
+  // separator between each of the values.  In this case the <br> element is used so that
+  // each log item is on it's own line.
+  const formattedLogs = logs.join("<br />");
+  const divLogElement = document.getElementById("logEntries");
+
+  // The innerHTML propery lets us inject a raw HTML string into the element for display on the page
+  divLogElement.innerHTML = formattedLogs;
 }
